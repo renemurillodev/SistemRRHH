3f28133fb4930af1b61166d0be38a46611eece38
ReneMurillo <rene.murillo95@hotmail.com>
2018-08-27 14:03:07 -0600

    Agregando la tabla Reportes

--- SistemaARD/Empleados.cs
+++ SistemaARD/Empleados.cs
@@ -24,6 +24,7 @@ namespace SistemaARD
             this.PlanillasProduccion = new HashSet<PlanillasProduccion>();
             this.PlanillasVentas = new HashSet<PlanillasVentas>();
             this.TelefonosEmpleados = new HashSet<TelefonosEmpleados>();
+            this.Reportes = new HashSet<Reportes>();
         }
     
         public int Id { get; set; }
@@ -59,5 +60,7 @@ namespace SistemaARD
         public virtual ICollection<PlanillasVentas> PlanillasVentas { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<TelefonosEmpleados> TelefonosEmpleados { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Reportes> Reportes { get; set; }
     }
 }
--- SistemaARD/LosNacimientos.Context.cs
+++ SistemaARD/LosNacimientos.Context.cs
@@ -44,5 +44,6 @@ namespace SistemaARD
         public virtual DbSet<TelefonosEmpleados> TelefonosEmpleados { get; set; }
         public virtual DbSet<TiposTelefonos> TiposTelefonos { get; set; }
         public virtual DbSet<Usuarios> Usuarios { get; set; }
+        public virtual DbSet<Reportes> Reportes { get; set; }
     }
 }
